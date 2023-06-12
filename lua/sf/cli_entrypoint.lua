589565621554998db327582ee3a8e65191008cb3
George Duffett <gduffett@certinia.com>
2023-06-12 11:35:20 +0100

    feat: add first pass scratch org user get

--- lua/sf/cli_entrypoint.lua
+++ lua/sf/cli_entrypoint.lua
@@ -11,5 +11,27 @@ vim.api.nvim_create_user_command('SF', function (opts)
 	local result = vim.fn.system(cmd)
 	vim.notify(result, {vim.log.levels.INFO}, {})
 end, { nargs = "+" })
+
+vim.api.nvim_create_user_command('SFInsertDefaultScratchOrgUser', function(opts)
+	local cmd = {'sf', 'org', 'list', '--json'}
+
+	local raw_output = vim.fn.system(cmd)
+
+	local result = vim.json.decode(raw_output)
+	local scratch_orgs = result['result']['scratchOrgs']
+
+	local scratch_org_username = ''
+	for scratch_org in scratch_orgs do
+		if scratch_org["isDefaultDevHubUsername"] == true then
+			scratch_org_username = scratch_org['username']
+		end
+	end
+
+	local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+	vim.api.nvim_buf_set_text(0, row - 1, col, row - 1, col, { scratch_org_username })
+end, {nargs = 0 })
+
 return M
+
+
 
