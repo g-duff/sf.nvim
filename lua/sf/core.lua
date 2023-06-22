bde167c322fd6fc672c2d732b49defde0bb8092c
George Duffett <gduffett@certinia.com>
2023-06-23 00:11:55 +0100

    refactor: function order in core module

--- lua/sf/core.lua
+++ lua/sf/core.lua
@@ -1,15 +1,5 @@
 local module = {}
 
-function module.sf_sync(sf_command_and_args)
-	local cmd = {'sf'}
-
-	for _, value in pairs(sf_command_and_args) do
-		table.insert(cmd, value)
-	end
-
-	return vim.fn.system(cmd)
-end
-
 function module.sf_sync_table(sf_command_and_args)
 	local cmd = {'sf'}
 
@@ -18,6 +8,16 @@ function module.sf_sync_table(sf_command_and_args)
 	raw_output = module.sf_sync(sf_command_and_args)
 
 	return vim.json.decode(raw_output)
+end
+
+function module.sf_sync(sf_command_and_args)
+	local cmd = {'sf'}
+
+	for _, value in pairs(sf_command_and_args) do
+		table.insert(cmd, value)
+	end
+
+	return vim.fn.system(cmd)
 end
 
 return module
