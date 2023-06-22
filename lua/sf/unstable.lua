9c79410a7445c40aada67711adf4254543996841
George Duffett <gduffett@certinia.com>
2023-06-23 00:39:43 +0100

    feat: add insert json command

--- lua/sf/unstable.lua
+++ lua/sf/unstable.lua
@@ -2,6 +2,18 @@ local M = {}
 
 local core = require('sf.core')
 
+vim.api.nvim_create_user_command('SFInsertJson', function (opts)
+	local raw_result = core.sf_sync_json(opts.fargs)
+
+	local json_lines = {}
+	for line in raw_result:gmatch("[^\r\n]+") do
+		table.insert(json_lines, line);
+	end
+
+	local row, _ = unpack(vim.api.nvim_win_get_cursor(0))
+	vim.api.nvim_buf_set_lines(0, row-1, row-1, false, json_lines)
+end, { nargs = "+" })
+
 vim.api.nvim_create_user_command('SFPrintScratchOrgDefaultUsername', function ()
 
 	local result = core.sf_sync_table({'org', 'list'})
